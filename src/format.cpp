c20519232f455bc1869cab900ff9998747d8d8b4
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-07-01 11:41:16 +0200

    fix: format to two digits the date

--- src/format.cpp
+++ src/format.cpp
@@ -4,6 +4,15 @@
 
 using std::string;
 using namespace std::chrono;
+
+//Verify length and convert to string
+string to_string(long value){
+    string string_value = std::to_string(value);
+    if(string_value.length()==1){
+        string_value = "0"+string_value;
+    }
+    return string_value;
+}
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long value) {
@@ -11,9 +20,6 @@ string Format::ElapsedTime(long value) {
     auto h = duration_cast<hours>(sec).count();
     auto m = duration_cast<minutes>(sec).count()%60;
     auto s = sec.count()%60;
-    string secs_string = std::to_string(s);
-    if(secs_string.length()==1){
-        secs_string = "0"+secs_string;
-    }
-    return std::to_string(h)+":"+std::to_string(m)+":"+secs_string;
+    return to_string(h)+":"+to_string(m)+":"+to_string(s);
 }
+
